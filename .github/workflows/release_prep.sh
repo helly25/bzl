135d222b4ba0869c10799bfe5af307e677fb2f89
helly25 <6420169+helly25@users.noreply.github.com>
2026-05-31 18:23:06 +0100

    Title is version not tag. Even though github will add a v1.2.3 tag.

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -91,7 +91,7 @@ git archive --format=tar.gz --prefix="${PREFIX}/" "${TAG}" -o "${ARCHIVE}" --add
 SHA256="$(shasum -a 256 "${ARCHIVE}" | awk '{print $1}')"
 
 # Print header
-echo "# Version ${TAG}"
+echo "# Version ${VERSION}"
 echo "## [Changelog](https://github.com/helly25/${PACKAGE_NAME}/blob/${TAG}/CHANGELOG.md)"
 
 # Print Changelog
